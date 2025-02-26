05b3a0923fe186068e35c026bdc1271c2c6caa39
unknown <aminskeh786@gmail.com>
2025-02-26 18:16:51 +0530

    project setup and first server run

---
+++ src/app/modules/sudent/student.interface.ts
@@ -0,0 +1,16 @@
+
+export type TStudent = {
+    id: string
+    name: {
+        firstName: string;
+        moddleName: string
+        lastName: string
+    };
+    gender: "male" | "female";
+    email: string;
+    dateOfBirth: string
+    contactNo:string;
+    emergencyContactNo:string;
+    bloodGrup?:
+
+}
