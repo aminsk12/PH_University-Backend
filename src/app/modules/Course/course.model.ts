fd64df4fbf77623ed3b6ddc2e1857b40fd9963e2
unknown <aminskeh786@gmail.com>
2025-04-22 19:53:13 +0530

    create course modal, interface, route, service, controller, validation

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,42 @@
+import { model, Schema } from "mongoose";
+import { TCourse, TPreRequisiteCourse } from "./course.interface";
+
+
+const preRequisiteCourseSchema = new Schema<TPreRequisiteCourse>({
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: "Course"
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const courseSchema = new Schema<TCourse>({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    prefix: {
+        type: String,
+        required: true,
+    },
+    code: {
+        type: Number,
+        required: true,
+    },
+    credits: {
+        type: Number,
+        required: true
+    },
+    preRequisiteCourses: [preRequisiteCourseSchema]
+},
+    {
+        timestamps: true
+    }
+)
+
+export const Course =  model<TCourse>('Course', courseSchema);
