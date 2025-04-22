039bd84dd63e04a7949d07ea1a6d70bba7597895
unknown <aminskeh786@gmail.com>
2025-04-22 12:27:05 +0530

    modefy global error

--- src/app/middlwares/globalErrorHandeler.ts
+++ src/app/middlwares/globalErrorHandeler.ts
@@ -4,19 +4,23 @@
 import { ErrorRequestHandler } from 'express';
 import config from '../config';
 
-const globalErrorHandler:ErrorRequestHandler = (
-    err,
-    req,
-    res,
-    next
-) => {
+type TErrorSource = {
+    path: string | number,
+    message: string
+}[]
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
+    const errorSorce:TErrorSource = [{
+        path: '',
+        message: 'Somthing went wrong'
+    }]
 
     res.status(statusCode).json({
         success: false,
         message,
-        error:err,
+        error: err,
         stack: config.NODE_ENV === 'devlopment' ? err.stack : null,
     });
 };
