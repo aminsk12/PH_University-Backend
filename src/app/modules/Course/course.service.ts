a84be96f3c96895ff3fb891a7a74bfb2554681b1
unknown <aminskeh786@gmail.com>
2025-05-17 20:45:44 +0530

    create course services

---
+++ src/app/modules/Course/course.service.ts
@@ -0,0 +1,30 @@
+import { Course } from "./course.model"
+
+
+const createCourseIntoDB = async () => {
+    const result = await Course.create();
+    return result
+}
+const getAllCoursesFromDB = async () => {
+    const result = await Course.find();
+    return result
+}
+const getSingleCourseFromDB = async (id: string) => {
+    const result = await Course.findById(id);
+    return result
+}
+const deletCourseFromDB = async (id: string) => {
+    const result = await Course.findByIdAndUpdate(
+        id,
+        { isDeleted: true },
+        { new: true }
+    );
+    return result
+}
+
+export const CourseService = {
+    createCourseIntoDB,
+    getAllCoursesFromDB,
+    getSingleCourseFromDB,
+    deletCourseFromDB
+}
